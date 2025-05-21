7cd1194882bb26db78278d7c2bcde7825abc0b5b
Shehzan Sheikh <75570393+shehzansk@users.noreply.github.com>
2025-05-21 14:20:12 +0530

    [74] [Time Beats: 100.00%] [Memory Beats: 18.47%] - LeetPush

--- leetcode/Search-a-2D-Matrix.java
+++ leetcode/Search-a-2D-Matrix.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row = matrix.length;
-        int col = matrix[0].length;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return false;
 
-        for(int i=0 ; i<row; i++){
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0, right = m * n - 1;
 
-            if(target <= matrix[i][col-1]){
-                for(int j =0; j<col; j++){
-                    if(target == matrix[i][j]) return true;
-                }
-            } 
-            
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midVal = matrix[mid / n][mid % n];
+
+            if (midVal == target) return true;
+            else if (midVal < target) left = mid + 1;
+            else right = mid - 1;
         }
 
         return false;
