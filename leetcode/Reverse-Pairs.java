8be446555c20fb3b39f5572144007dade67e57e5
Shehzan Sheikh <sshahzan0@gmail.com>
2025-05-27 21:23:31 +0530

    [493] [Time Beats: 23.92%] [Memory Beats: 96.66%] - LeetPush

---
+++ leetcode/Reverse-Pairs.java
@@ -0,0 +1,72 @@
+class Solution {
+
+    private static int merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>(); // temporary array
+        int left = low;      // starting index of left half of arr
+        int right = mid + 1;   // starting index of right half of arr
+
+        //Modification 1: cnt variable to count the pairs:
+        int cnt = 0;
+        int j = mid + 1;
+
+        // Count reverse pairs:
+        for (int i = low; i <= mid; i++) {
+            while (j <= high && arr[i] > 2L * arr[j]) {
+                j++;
+            }
+            cnt += (j - (mid + 1));
+        }
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+
+        // if elements on the left half are still left //
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        //  if elements on the right half are still left //
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        // transfering all elements from temporary to arr //
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+        return cnt; // Modification 3
+    }
+
+    public static int mergeSort(int[] arr, int low, int high) {
+        int cnt = 0;
+        if (low >= high) return cnt;
+        int mid = (low + high) / 2 ;
+        cnt += mergeSort(arr, low, mid);  // left half
+        cnt += mergeSort(arr, mid + 1, high); // right half
+        cnt += merge(arr, low, mid, high);  // merging sorted halves
+        return cnt;
+    }
+    
+    public int reversePairs(int[] nums) {
+        int n = nums.length;
+        return mergeSort(nums, 0, n-1);
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     for (int j = i + 1; j < nums.length; j++) {
+        //         if (nums[i]/2.0 >  nums[j])
+        //             cnt++;
+        //     }
+        // }
+
+    }
+}
