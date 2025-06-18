540a25ae6f715249214c9880f9ef21878eef63c9
Shehzan Sheikh <sshahzan0@gmail.com>
2025-06-18 16:00:57 +0530

    [2966] [Time Beats: 98.43%] [Memory Beats: 75.92%] - LeetPush

--- leetcode/Divide-Array-Into-Arrays-With-Max-Difference.java
+++ leetcode/Divide-Array-Into-Arrays-With-Max-Difference.java
@@ -3,35 +3,46 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         int[][] ans = new int[n/3][];
-        int[] visited = new int[n];
-        Arrays.fill(visited, 0);
+        // int[] visited = new int[n];
+        // Arrays.fill(visited, 0);
 
-        int m = 0;
+        // int m = 0;
 
-        for(int i=0; i<n; i++){
-            if (visited[i] == 1) continue;
-            ans[m] = new int[3];
-            ans[m][0] = nums[i];
-            visited[i] = 1;
-            int l = 1;
-            int min = nums[i], max = nums[i];
-            for(int j = i+1; j<n; j++){
-                if (visited[j] == 0) {
-                    int newMin = Math.min(min, nums[j]);
-                    int newMax = Math.max(max, nums[j]);
-                    if (newMax - newMin <= k) {
-                        ans[m][l++] = nums[j];
-                        visited[j] = 1;
-                        min = newMin;
-                        max = newMax;
-                    }
-                }
-                if (l == 3) break;
+        // for(int i=0; i<n; i++){
+        //     if (visited[i] == 1) continue;
+        //     ans[m] = new int[3];
+        //     ans[m][0] = nums[i];
+        //     visited[i] = 1;
+        //     int l = 1;
+        //     int min = nums[i], max = nums[i];
+        //     for(int j = i+1; j<n; j++){
+        //         if (visited[j] == 0) {
+        //             int newMin = Math.min(min, nums[j]);
+        //             int newMax = Math.max(max, nums[j]);
+        //             if (newMax - newMin <= k) {
+        //                 ans[m][l++] = nums[j];
+        //                 visited[j] = 1;
+        //                 min = newMin;
+        //                 max = newMax;
+        //             }
+        //         }
+        //         if (l == 3) break;
                 
-            }
-            if(l != 3) return new int[0][0];
+        //     }
+        //     if(l != 3) return new int[0][0];
 
-            m++;
+        //     m++;
+        // }
+
+
+        for(int i=0 ; i<n ; i+=3){
+            int temp[] = Arrays.copyOfRange(nums, i, i+3);
+
+            if(temp[2] - temp[0] >k){
+                return new int[0][0];
+            }
+
+            ans[i/3] = temp;
         }
 
         return ans;
