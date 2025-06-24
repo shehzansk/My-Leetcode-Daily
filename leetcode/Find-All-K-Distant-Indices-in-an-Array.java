57bb0f0b9e92c190a6298c6bbbf53362fe356881
Shehzan Sheikh <sshahzan0@gmail.com>
2025-06-24 12:06:49 +0530

    [2200] [Time Beats: 65.56%] [Memory Beats: 51.24%] - LeetPush

--- leetcode/Find-All-K-Distant-Indices-in-an-Array.java
+++ leetcode/Find-All-K-Distant-Indices-in-an-Array.java
@@ -1,28 +1,50 @@
 class Solution {
     public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
-        Set<Integer> st  = new HashSet<>();
+        // Set<Integer> st  = new HashSet<>();
+        // int n = nums.length;
+
+        // List<Integer> fq = new ArrayList<>();
+
+
+        // for(int i=0 ;i<n; i++){
+        //     if(nums[i] == key){
+        //         fq.add(i);
+        //     }
+        // }
+        // // int j = 0;
+
+        // for(int i=0; i<n; i++){
+        //     for(int f:fq){
+        //         if(Math.abs(i-f) <=k){
+        //             st.add(i);
+        //         }
+        //     }
+        // }
+
+        // List<Integer> ans = new ArrayList<>(st);
+        // Collections.sort(ans);
+        // return ans;
+
+        //Optimizing - 1:
+
+        List<Integer> ans = new ArrayList<>();
         int n = nums.length;
-
         List<Integer> fq = new ArrayList<>();
-
 
         for(int i=0 ;i<n; i++){
             if(nums[i] == key){
                 fq.add(i);
             }
         }
-        // int j = 0;
 
         for(int i=0; i<n; i++){
             for(int f:fq){
                 if(Math.abs(i-f) <=k){
-                    st.add(i);
+                    ans.add(i);
+                    break;
                 }
             }
         }
-
-        List<Integer> ans = new ArrayList<>(st);
-        Collections.sort(ans);
         return ans;
     }
 }
