63746d21113c523b60787b65a73fa94fcd4fc15d
Shehzan Sheikh <sshahzan0@gmail.com>
2025-07-06 01:03:55 +0530

    [1394] [Time Beats: 99.94%] [Memory Beats: 72.16%] - LeetPush

--- leetcode/Find-Lucky-Integer-in-an-Array.java
+++ leetcode/Find-Lucky-Integer-in-an-Array.java
@@ -1,26 +1,38 @@
 class Solution {
     public int findLucky(int[] arr) {
 
-        int n = arr.length;
-        int lucky = -1;
+        // int n = arr.length;
+        // int lucky = -1;
 
-        Map<Integer, Integer> fq = new HashMap<>();
+        // Map<Integer, Integer> fq = new HashMap<>();
 
-        for(int a: arr){
-            fq.put(a, fq.getOrDefault(a, 0)+1);
+        // for(int a: arr){
+        //     fq.put(a, fq.getOrDefault(a, 0)+1);
+        // }
+
+        // int l = -1;
+
+        // for(Map.Entry<Integer, Integer> e : fq.entrySet()){
+        //     if(e.getKey() == e.getValue()){
+        //         l = e.getKey();
+        //     }
+
+        //     lucky = Math.max(lucky, l);
+        // }
+
+        // return lucky;
+
+        int[] freq = new int[501]; 
+
+        for (int num : arr) {
+            freq[num]++;
         }
 
-        int l = -1;
-
-        for(Map.Entry<Integer, Integer> e : fq.entrySet()){
-            if(e.getKey() == e.getValue()){
-                l = e.getKey();
-            }
-
-            lucky = Math.max(lucky, l);
+        for (int i = 500; i >= 1; i--) {
+            if (freq[i] == i) return i;
         }
 
-        return lucky;
+        return -1;
         
     }
 }
